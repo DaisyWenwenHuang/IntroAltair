4f98729db4413f95751d3b7cbaaaade52fa4cb87
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2023-01-19 14:04:29 +0000

    scatter plots

--- intro.py
+++ intro.py
@@ -4,10 +4,28 @@ import altair as alt
 data = pd.read_csv("WHR_2016.csv")
 data.head()
 
+# bar plot
 alt.Chart(data).mark_bar().encode(
 	x = 'Region',
 	y = 'Happiness Score'
 )
 
+# scatter plot 1
+healthPlot = alt.Chart(data).mark_circle().encode(
+	x = 'Health(Life Expectancy)',
+	y = 'Happiness Score',
+	color = alt.Color('Region',scale = alt.Scale(scheme='spectral')),
+	tooltip = ['Country','Happiness Score'],
+)
 
+# scatter plot 2
+generosityPlot = alt.Chart(data).mark_circle().encode(
+	x = 'Generosity',
+	y = 'Happiness Score',
+	color = alt.Color('Region',scale = alt.Scale(scheme='spectral')),
+	tooltip = ['Country','Happiness Score'],
+)
+
+# focet scatter plot 1 and 2
+healthPlot | generosityPlot
 
