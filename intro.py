a6755ff0a00517812f0f29515771752101f7fe6c
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2023-01-19 14:00:16 +0000

    bar plot

--- intro.py
+++ intro.py
@@ -1,3 +1,13 @@
 import pandas as pd
+import altair as alt
+# world happiness data 2016
 data = pd.read_csv("WHR_2016.csv")
 data.head()
+
+alt.Chart(data).mark_bar().encode(
+	x = 'Region',
+	y = 'Happiness Score'
+)
+
+
+
